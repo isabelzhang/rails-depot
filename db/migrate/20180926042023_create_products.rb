084a2816636bcf06ea266ce81ac6b311d5cf8d4a
Isabel Zhang <isabelzhang@berkeley.edu>
2018-09-25 22:52:37 -0700

    Depot Scaffold

---
+++ db/migrate/20180926042023_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price, precision: 8, scale: 2 # 8 sig digits, 2 digits after decimal
+
+      t.timestamps
+    end
+  end
+end
